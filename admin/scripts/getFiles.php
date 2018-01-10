1778e627dfc5d9812336d509cb1d8ea71a51c16f
unknown <vigeasmos@gmail.com>
2018-01-10 15:00:44 -0500

    database base minus filters

--- admin/scripts/getFiles.php
+++ admin/scripts/getFiles.php
@@ -6,16 +6,23 @@
 	//Connect to database
 	include('connect.php');
 
-	if(isset($_GET['filter'])){
+	if(isset($_GET['type'])){
 		//1. Filters movies by genre
-		$filter = $_GET['filter'];
-		$filterQuery = "SELECT p.port_title, p.port_position, p.port_summary, p.port_image, p.port_outside, o.outside_name FROM tbl_portfolio p, tbl_outside o, tbl_port_out po, tbl_type t, tbl_port_type pt WHERE p.port_id = po.port_id AND o.outside_id = po.outside_id AND p.port_id = pt.port_id AND t.type_id = pt.type_id AND t.type_name = {$filter}";
+		$type = $_GET['type'];
+		$typeQuery = "SELECT p.port_title, p.port_position, p.port_summary, p.port_image, p.port_outside, o.outside_name FROM tbl_portfolio p, tbl_outside o, tbl_port_out po, tbl_type t, tbl_port_type pt WHERE p.port_id = po.port_id AND o.outside_id = po.outside_id AND p.port_id = pt.port_id AND t.type_id = pt.type_id AND t.type_name = {$type}";
 		//echo $filterQuery;
-		$getFiles = mysqli_query($link, $filterQuery);
-	}else if(isset($_GET['id'])){
+		$getFiles = mysqli_query($link, $typeQuery);
+	}else if(isset($_GET['stage'])){
+		//1. Filters movies by genre
+		$stage = $_GET['stage'];
+		$stageQuery = "SELECT p.port_title, p.port_position, p.port_summary, p.port_image, p.port_outside, o.outside_name FROM tbl_portfolio p, tbl_outside o, tbl_port_out po, tbl_stages s, tbl_port_stag ps WHERE p.port_id = po.port_id AND o.outside_id = po.outside_id AND p.port_id = ps.port_id AND s.stages_id = ps.stages_id AND s.stages_name = {$stage}";
+		//echo $filterQuery;
+		$getFiles = mysqli_query($link, $stageQuery);
+	}
+	else if(isset($_GET['id'])){
 		//2. Return a single movie
 		$id = $_GET['id'];
-		$querySingle = "SELECT p.port_title, p.port_position, p.port_details, p.port_date, p.port_image, p.port_outside, o.outside_name, GROUP_CONCAT(pr.program_name) AS 'program_name' FROM tbl_portfolio p, tbl_outside o, tbl_port_out po, tbl_programs pr, tbl_port_prog pp WHERE p.port_id = po.port_id AND o.outside_id = po.outside_id AND p.port_id = pp.port_id AND pr.program_id = pp.program_id AND p.port_id = {$id}";
+		$querySingle = "SELECT p.port_title, p.port_position, p.port_details, p.port_date, p.port_image, p.port_outside, o.outside_name, GROUP_CONCAT(pr.program_name SEPARATOR ', ') AS 'program_name' FROM tbl_portfolio p, tbl_outside o, tbl_port_out po, tbl_programs pr, tbl_port_prog pp WHERE p.port_id = po.port_id AND o.outside_id = po.outside_id AND p.port_id = pp.port_id AND pr.program_id = pp.program_id AND p.port_id = {$id}";
 		$getFiles = mysqli_query($link, $querySingle);
 	}else{
 		//4. Returns all movies
--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 (function() {
 
-    /*//filter menu
-    var menu = document.querySelector("#filterMenu"),
+    //filter menu
+    /*var menu = document.querySelector("#filterMenu"),
         listTime = menu.querySelector("#timeMenu"),
         listStage = menu.querySelector("#stageMenu"),
         listType = menu.querySelector("#typeMenu"),
@@ -11,9 +11,9 @@
         appliedFilterTi,
         appliedFilterSt,
         appliedFilterTy,
-        appliedArray;*/
+        appliedArray;
 
-    /*function changeOptions(evt) {
+    function changeOptions(evt) {
         appliedArray=evt.currentTarget.parentElement.parentElement;
         if(appliedFilterTi===evt.currentTarget || appliedFilterSt===evt.currentTarget || appliedFilterTi===evt.currentTarget) {
             evt.currentTarget.classList.remove("activeFilter");
@@ -51,7 +51,7 @@
         element.addEventListener('click', changeOptions, false);
     });*/
 
-  
+    //gallery
       const httpRequest = new XMLHttpRequest();
         //1. make an AJAX call to the databse; handle any errors first
         if (!httpRequest) {
@@ -81,36 +81,9 @@
         }
     }
     
-         /* function processResult(data) {
-            //debugger;
-            const { modelName, pricing, modelDetails} = data;
-            // statement chaining lets you go lots of stuff on one line
-            let model = document.querySelector('.modelName').textContent = modelName;
-            let price = document.querySelector('.priceInfo').innerHTML = pricing;
-            let details = document.querySelector('.modelDetails').textContent = modelDetails;
-            //debugger;
-            //changeModel text
-            let objectIndex = carData[this.id];
-        
-            //model.firstChild.nodeValue = objectIndex.model;
-            //price.firstChild.nodeValue = objectIndex.price;
-            //details.firstChild.nodeValue = objectIndex.description;
-        
-            //opacity change
-        
-            Img.forEach(function(element, index) {
-              element.classList.remove("focusMini");
-            });
-            // the backticks are a new ES6 thing called a template string (look it up)
-            document.querySelector(`#${data.model}`).classList.add("focusMini");
-        
-            //console.log(this.id);
-          }*/
-            
-            //outsideL.style.color = "#00ffff";
 
         
-          var lb, lbImg;
+
 
       function processResult(data) {
         //debugger;
@@ -127,23 +100,14 @@
                 let image = cln.querySelector('.lbImg').src = "images/gallery/" + data[index].port_image + "-large.jpg";
                 let outside = cln.querySelector('.outsideLink a').innerHTML = "View on " + data[index].outside_name;
                 let outsideL = cln.querySelector('.outsideLink a').href = data[index].port_outside;
-                lb = cln.querySelector(".lbLink");
-                lbImg = cln.querySelector(".lbImg");
+                let lb = cln.querySelector(".lbLink");
+                let lbImg = cln.querySelector(".lbImg");
 
                 lb.id = index + 1;
                 lbImg.id = index + 1;
 
                 lb.addEventListener("click", popLightbox, false);
                 lbImg.addEventListener("click", popLightbox, false);
-        
-                // add an index number to the thumbnail for array reference
-                //lb.dataset.index = index;
-                //lbImg.dataset.index = index;
-                /*lb.id = index;
-                lbImg.id = index;
-                console.log(lb.id);*/
-
-                //console.log(lb.dataset.index);
         
                 // append it to the container
                 gallery.appendChild(cln);
@@ -161,12 +125,6 @@
         window.scrollTo(0, 0);
         document.body.style.overflow = "hidden";
 
-        let lightbox = document.querySelector('.lightbox');
-        lightbox.style.display = 'block';
-
-        let lightboxClose = lightbox.querySelector('.close-lightbox');
-        lightboxClose.addEventListener('click', closeLightbox, false);
-
         const httpRequest = new XMLHttpRequest();
         //1. make an AJAX call to the databse; handle any errors first
         if (!httpRequest) {
@@ -174,29 +132,32 @@
           return false;
         }
     
-        httpRequest.onreadystatechange = processRequestL(this.id);
+        httpRequest.onreadystatechange = processRequestL;
         httpRequest.open('GET', 'admin/scripts/getFiles.php?id=' + this.id);
         httpRequest.send();
     
-        function processRequestL(id) {
+        function processRequestL() {
             // handle the stages of our AJAX call
             let reqIndicator = document.querySelector('.request-state');
             reqIndicator.textContent = httpRequest.readyState;
         
-            //debugger;
-        
             if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if (httpRequest.status === 200) {// request worked, all is good
-                //debugger;
                 let data = JSON.parse(httpRequest.responseText);
-                console.log(data);
                 processResultLight(data);
             } else {
                 alert('There was a problem with the request');
             }
-            debugger;
             }
         }
+
+        let lightbox = document.querySelector('.lightbox');
+        lightbox.style.display = 'block';
+
+        let lightboxClose = lightbox.querySelector('.close-lightbox');
+        lightboxClose.addEventListener('click', closeLightbox, false);
+
+        
     }
 
     function closeLightbox() {
@@ -210,21 +171,95 @@
 
     function processResultLight(data) {
         //debugger;
-        //let id = this.dataset.index;
-        console.log("data");
+
         // statement chaining lets you go lots of stuff on one line
         let detailsBox = document.querySelector(".gDetails");
         const [{port_title, port_position, port_details, port_date, port_image, port_outside, outside_name, program_name}] = data;
         let title = detailsBox.querySelector('.title').innerHTML = port_title;
         let position = detailsBox.querySelector('.position').innerHTML = port_position;
-        let details = detailsBox.querySelector('.details').innerHTML = port_details;
+        let details = detailsBox.querySelector('.date').innerHTML = port_date;
+        let date = detailsBox.querySelector('.details').innerHTML = port_details;
         let image = detailsBox.querySelector('.imgS').src = "images/gallery/" + port_image + ".jpg";
         let imageM = detailsBox.querySelector('.imgM').src = "images/gallery/" + port_image + ".jpg";
-        let outside = detailsBox.querySelector('.outsideLink a').innerHTML = "View on " + outside_name;
-        let outsideL = detailsBox.querySelector('.outsideLink a').href = port_outside;
+        let outside = detailsBox.querySelector('.outsideLink').innerHTML = "View on " + outside_name;
+        let outsideL = detailsBox.querySelector('.outsideLink').href = port_outside;
+        console.log(program_name);
         let program = detailsBox.querySelector('.program h6').innerHTML = program_name;
 
     
         //console.log(this.id);
     }
+
+    //filters
+    var listStage = document.querySelector("#stageMenu"),
+        listType = document.querySelector("#typeMenu"),
+        optionsSt = listStage.querySelectorAll("li a"),
+        optionsTy = listType.querySelectorAll("li a");
+    
+        function changeOptionsSt() {
+            
+            const httpRequest = new XMLHttpRequest();
+            //1. make an AJAX call to the databse; handle any errors first
+            if (!httpRequest) {
+                alert('giving up, your browser sucks!');
+                return false;
+            }
+            
+            httpRequest.onreadystatechange = processRequestF;
+            httpRequest.open('GET', 'admin/scripts/getFiles.php?stage="' + this.id + '"');
+            httpRequest.send();
+            debugger;
+        }
+
+        function changeOptionsTy() {
+            const httpRequest = new XMLHttpRequest();
+            if (!httpRequest) {
+                alert('giving up, your browser sucks!');
+                return false;
+            }
+
+            httpRequest.onreadystatechange = processRequestF;
+            httpRequest.open('GET', 'admin/scripts/getFiles.php?type="' + this.id + '"');
+            httpRequest.send();
+        }
+
+        function processResultF(data) {
+            //debugger;
+            
+            // statement chaining lets you go lots of stuff on one line
+            let gallery = document.querySelector('#gallery');
+            var file = document.querySelector(".gFile");
+                data.forEach(function(element, index){
+                    const [{port_title, port_position, port_summary, port_image, port_outside, outside_name}] = data;
+                    let cln = file.cloneNode(true);
+                    let title = cln.querySelector('.title').innerHTML = data[index].port_title;
+                    let position = cln.querySelector('.position').innerHTML = data[index].port_position;
+                    let summary = cln.querySelector('.summary').innerHTML = data[index].port_summary;
+                    let image = cln.querySelector('.lbImg').src = "images/gallery/" + data[index].port_image + "-large.jpg";
+                    let outside = cln.querySelector('.outsideLink a').innerHTML = "View on " + data[index].outside_name;
+                    let outsideL = cln.querySelector('.outsideLink a').href = data[index].port_outside;
+                    let lb = cln.querySelector(".lbLink");
+                    let lbImg = cln.querySelector(".lbImg");
+    
+                    lb.id = index + 1;
+                    lbImg.id = index + 1;
+    
+                    lb.addEventListener("click", popLightbox, false);
+                    lbImg.addEventListener("click", popLightbox, false);
+            
+                    // append it to the container
+                    gallery.appendChild(cln);
+                });
+    
+            file.classList.add('hide');
+        
+            //console.log(this.id);
+            }
+
+        optionsSt.forEach(function(element, index) {
+            element.addEventListener('click', changeOptionsSt, false);
+        });
+        optionsTy.forEach(function(element, index) {
+            element.addEventListener('click', changeOptionsTy, false);
+        });
 })();
